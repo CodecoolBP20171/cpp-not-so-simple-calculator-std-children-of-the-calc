af28b9803cc44211ce303ce86e7fc394d1aa9a47
Bán Marcell <afga93+cc@gmail.com>
2017-09-27 14:30:38 +0200

    Added user interface

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include "Tester.hpp"
 
+namespace {
+    void printHelp(){
+        std::cout << "To get to this help screen type 'help'." << std::endl;
+        std::cout << "To quit this !!!awesome!!! program type 'exit'." << std::endl;
+        std::cout << "To run automated tests type 'test'." << std::endl;
+        std::cout << "Other inputs will be treated as mathematical expressions and will be evaluated." << std::endl;
+    }
+}
+
 int main(){
     Tester t;
-    t.run();
+    Calculator c;
+    bool running = true;
+    std::cout << "NoSoSiCalc (for help type 'help')" << std::endl;
+    do{
+        std::cout << "$> ";
+        std::string input;
+        getline(std::cin, input);
+        if(input == "exit") running = false;
+        else if(input == "help") printHelp();
+        else if (input == "test") t.run();
+        else std::cout << c.evaluate(input) << std::endl;
+    } while(running);
+    std::cout << "Thanks, Goodbye!" << std::endl;
     return 0;
 }
